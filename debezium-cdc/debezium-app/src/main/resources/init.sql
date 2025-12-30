6625fb0eea79816910105ef7b43abf15e8418208
Precyzik <gawaprecyz@gmail.com>
2025-12-30 12:34:37 +0100

    Moving offset and history to the db.

---
+++ debezium-cdc/debezium-app/src/main/resources/init.sql
@@ -0,0 +1,23 @@
+CREATE TABLE `cdc`.`.database_history`
+(
+    `id`                VARCHAR(36) NOT NULL,
+    `history_data`      MEDIUMTEXT,
+    `history_data_seq`  BIGINT NOT NULL,
+    `record_insert_ts`  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `record_insert_seq` BIGINT NOT NULL,
+    PRIMARY KEY (`id`, `history_data_seq`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
+
+CREATE TABLE `cdc`.`offset_storage`
+(
+    `id`                VARCHAR(36) NOT NULL,
+    `offset_key`        TEXT DEFAULT NULL,
+    `offset_val`        TEXT DEFAULT NULL,
+    `record_insert_ts`  TIMESTAMP NOT NULL,
+    `record_insert_seq` BIGINT NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
--- debezium-cdc/debezium-core/src/main/java/pawg/hexagonal/cdc/config/CdcConfiguration.java
+++ debezium-cdc/debezium-core/src/main/java/pawg/hexagonal/cdc/config/CdcConfiguration.java
@@ -1,47 +1,24 @@
 package pawg.hexagonal.cdc.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.*;
 
 @Configuration
-@Profile({"cdc"})
+@EnableConfigurationProperties
 public class CdcConfiguration {
+    private final Map<String, String> debeziumConfiguration = new HashMap<>();
+
+    @Bean(name = "debeziumConfiguration")
+    @ConfigurationProperties(prefix = "debezium.configuration")
+    public Map<String, String> debeziumConfiguration() {
+        return debeziumConfiguration;
+    }
 
     @Bean
-    public io.debezium.config.Configuration customerConnector(@Value("${datasource.hostname}") String customerDbHost,
-                                                              @Value("${datasource.port}") String customerDbPort,
-                                                              @Value("${datasource.username}") String customerDbUsername,
-                                                              @Value("${datasource.password}") String customerDbPassword,
-                                                              @Value("${datasource.dbName}") String customerDbName) {
-
-        return io.debezium.config.Configuration.create()
-                .with("name", "backend-mysql-connector")
-                .with("connector.class", "io.debezium.connector.mysql.MySqlConnector")
-
-                .with("offset.storage", "org.apache.kafka.connect.storage.FileOffsetBackingStore")
-                .with("offset.storage.file.filename", "./offsets.dat")
-                .with("offset.flush.interval.ms", "60000")
-
-                .with("driver.serverTimezone", "UTC")
-                .with("driver.connectionTimeZone", "UTC")
-                .with("database.hostname", customerDbHost)
-                .with("database.port", customerDbPort)
-                .with("database.user", customerDbUsername)
-                .with("database.password", customerDbPassword)
-                .with("database.dbname", customerDbName)
-                .with("database.server.id", "10181")
-//                .with("database.server.name", "backend-mysql-db-server")
-//                .with("database.include.list", customerDbName)
-                .with("database.exclude.list", customerDbName)
-//                .with("include.schema.changes", "false")
-
-                .with("topic.prefix", "backend-cdc")
-
-                .with("schema.history.internal", "io.debezium.storage.file.history.FileSchemaHistory")
-                .with("schema.history.internal.file.filename", "./schemahistory.dat")
-
-                .with("snapshot.mode", "initial")
-                .with("snapshot.locking.mode", "none")
-                .build();
+    public io.debezium.config.Configuration customerConnector(final Map<String, String> debeziumConfiguration) {
+        return io.debezium.config.Configuration.create().apply((b) -> debeziumConfiguration.forEach(b::with)).build();
     }
 }
