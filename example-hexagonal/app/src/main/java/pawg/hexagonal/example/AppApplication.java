315c4e748232db2025640492d904b4d0f40cccb8
Precyzik <gawaprecyz@gmail.com>
2026-04-14 08:04:10 +0200

    Switching on logs for grpc.

--- example-hexagonal/app/src/main/java/pawg/hexagonal/example/AppApplication.java
+++ example-hexagonal/app/src/main/java/pawg/hexagonal/example/AppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppApplication {
 
     static void main(String[] args) {
+        System.setProperty("org.slf4j.simpleLogger.log.org.openjproxy.grpc.client", "debug");
         SpringApplication.run(AppApplication.class, args);
     }
 
